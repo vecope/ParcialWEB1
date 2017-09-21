b984bd78c43f91a0a61bcf964cb8285e99fc9992
Camilo Alfonso <ca.alfonso967@uniandes.edu.co>
2017-09-21 15:20:34 -0500

    Git workin'

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var gitHubApi = require ("github");
 
+/* GET home page. */
+var GitHubApi = require("github");
 
-router.get('/getFollowers/:user', function(){
+router.get('/followers/:user', function(req, res, next) {
 
+    var github = new GitHubApi({});
 
-  var github = new GitHubApi({});
-
-  github.users.getFollowersForUser({
-    username:req.params.user}, function ( err, data) {
-      res.json(data);
-  });
-
+    github.users.getFollowingForUser({
+        username: req.params.user
+    }, function(err, data) {
+        res.json(data)
+    });
 });
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
